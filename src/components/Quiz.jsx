b21e4b4753276f4f282b9ba41feaa94be524471f
Adam Evans <adam3van5@gmail.com>
2024-08-18 22:13:40 +0900

    derive state

--- src/components/Quiz.jsx
+++ src/components/Quiz.jsx
@@ -1,7 +1,8 @@
 import { useState } from 'react';
 export default function Quiz() {
-  const [activeQuestion, setActiveQuestion] = useState(0);
   const [userAnswers, setUserAnswers] = useState([]);
+  const activeQuestionIndex = userAnswers.length;
+
   return (
     <div>
       <p>Currently active question</p>
